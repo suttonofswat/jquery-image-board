8ed41f2643b917ce32832d655fe75ab4235a3f3a
Leslie Sutton <l4sutton@gmail.com>
2015-09-18 16:39:07 -0500

    sign up login forms working and displaying username

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -11,14 +11,60 @@ $(document).ready(function () {
 	var $imgList = $('#imgList');
 	var $imgForm = $('#imgForm');
 	var $error = $('#error');
+	var $loginForm = $('#loginForm');
+	var $username = $('#username');
+	var $password = $('#password');
+	var $loginBG = $('.loginBG');
+	var $suBtn = $('#suBtn');
+	var $suForm = $('#suForm');
+	var $suUN = $('#suUN');
+	var $suPW = $('#suPW');
+	var $name = $('.username');
+
 	var url = 'http://tiyfe.herokuapp.com/collections/fee';
+	var usersUrl = 'http://tiyfe.herokuapp.com/collections/feeUsers';
 
 	$imgForm.hide();
+	$suForm.hide();
+
+	$loginForm.submit(function (e) {
+		e.preventDefault();
+		var newUN = $username.val();
+		var newPW = $password.val();
+
+		console.log(newUN);
+		console.log(newPW);
+
+		$.get(usersUrl, function (response) {
+			for (var i = 0; i < response.length; i++) {
+				if (response[i].username === newUN && response[i].password === newPW) {
+					$loginBG.toggle('down');
+				}
+			}
+		}, 'json');
+	});
+	$suBtn.click(function () {
+		$loginForm.toggle('down');
+		$suForm.toggle('up');
+	});
+
+	$suForm.submit(function (e) {
+		e.preventDefault();
+		var newSU = $suUN.val();
+		var newSUPw = $suPW.val();
+
+		$.post(usersUrl, {
+			username: newSU,
+			password: newSUPw
+		}, 'json');
+		$loginBG.hide();
+	});
+
 	setInterval(function () {
 		$.get(url, function (response) {
 			$imgList.html('');
 			response.forEach(function (response) {
-				$imgList.append('<div class="full"><div><img class="actualPic" src="' + response.photo + '"></div><div class="comments">' + response.caption + '</div></div>');
+				$imgList.append('<div class="full col-md-offset-1 col-md-3 col-lg-2"><div><img class="actualPic img-responsive" src="' + response.photo + '"></div><div class="person">' + response.username + '</div><div class="comments">' + response.caption + '</div></div>');
 			});
 		}, 'json');
 	}, 500);
@@ -32,11 +78,14 @@ $(document).ready(function () {
 		e.preventDefault();
 		var newImg = $imgUrl.val();
 		var newDesc = $imgDesc.val();
+		var newName = $name.val();
 
 		if ((newImg.indexOf('png') !== -1 || newImg.indexOf('jpg') !== -1 || newImg.indexOf('gif') !== -1) && newDesc !== '') {
 			$.post(url, {
+				username: newName,
 				photo: newImg,
 				caption: newDesc
+
 			}, 'json');
 			$imgUrl.val('');
 			$imgDesc.val('');
